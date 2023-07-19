c290ab0f0cae92df61562d43ee12869f5d7aa9e4
romankrutel <138224285+romankrutel@users.noreply.github.com>
2023-07-19 18:37:38 +0300

    Add files via upload

---
+++ roman123/zadacha19/Program.cs
@@ -0,0 +1,15 @@
+﻿// Проверка, является ли пятизначное число палиндромом.
+Console.WriteLine("Введите число");
+string number = Console.ReadLine();
+int len = number.Length;
+if (len == 5)
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine($"{number} - Палиндром");
+    }
+    else
+    {
+        Console.WriteLine($"{number} - НЕ палиндром");
+    }
+}
---
+++ roman123/zadacha21/Program.cs
@@ -0,0 +1,16 @@
+﻿// Программа,  которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+int x1 = 3;
+int y1 = 6;
+int z1 = 8;
+
+int x2 = 2;
+int y2 = 1;
+int z2 = -7;
+
+int a = x2 - x1;
+int b = y2 - y1;
+int c = z1 - z2;
+
+double length = Math.Sqrt(a * a + b * b + c * c);
+Console.WriteLine($"Длинна отрезка {length}");
